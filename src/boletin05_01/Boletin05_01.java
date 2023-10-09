43a3bf9d9aa6cab4d606a9466c4b733af67d905a
cod3stealer <sromerodiaz@danielcastelao.org>
2023-10-09 10:32:24 +0200

    Boletin 5

---
+++ src/boletin05_01/Boletin05_01.java
@@ -0,0 +1,43 @@
+package boletin05_01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin05_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce kilometros : ");
+        float km = sc.nextFloat();
+        System.out.print("Introduce litros : ");
+        float litros = sc.nextFloat();
+        System.out.print("Introduce velocidad media : ");
+        float vMed = sc.nextFloat();
+        System.out.print("Introduce precio gasolina : ");
+        float pGas = sc.nextFloat();
+        // Constructor
+        consumo consume = new consumo();
+        consume.setLitros(litros);
+        consume.setpGas(pGas);
+        consume.setKms(km);
+        consume.setvMed(vMed);
+        System.out.println("Consumo medio = "+consume.consumoMedio()+" litros/km");
+        System.out.println("Velocidade media = "+consume.getvMed()+" Km/h");
+        System.out.println("Tiempo viaje = "+consume.getTempo()+" Horas");
+
+        // Constructor parametrizado
+        consumo consumo = new consumo(km,litros,vMed,pGas);
+        //consumo.consumo(km,litros,vMed,pGas);
+        System.out.println("Consumo medio = "+consumo.consumoMedio()+" litros/km");
+        System.out.println("Velocidade media = "+consumo.getvMed()+" Km/h");
+        System.out.println("Tiempo viaje = "+consume.getTempo()+" Horas");
+    }
+
+}
---
+++ src/boletin05_01/consumo.java
@@ -0,0 +1,62 @@
+package boletin05_01;
+
+/**
+ *
+ * @author dam1
+ */
+public class consumo {
+    private float km;
+    private float litros;
+    private float vMed;
+    private float pGas;
+
+    public consumo() {
+
+    }
+
+    public consumo(float km, float litros, float vMed, float pGas) {
+        this.km = km;
+        this.litros = litros;
+        this.vMed = vMed;
+        this.pGas = pGas;
+    }
+
+    public float getTempo() {
+        return km/vMed;
+    }
+
+    public int consumoMedio() {
+        return (int) ((litros/km)*100);
+    }
+
+    public float consumoEuros() {
+        return (pGas/km)*100;
+    }
+
+    public void setKms(float km) {
+        this.km = km;
+    }
+    public void setLitros(float litros) {
+        this.litros = litros;
+    }
+    public void setvMed(float vMed) {
+        this.vMed = vMed;
+    }
+    public void setpGas(float pGas) {
+        this.pGas = pGas;
+    }
+
+    public float getKms() {
+        return this.km;
+    }
+    public float getLitros() {
+        return this.litros;
+    }
+    public float getvMed() {
+        return this.vMed;
+    }
+    public float getpGas() {
+        return this.pGas;
+    }
+
+}
